fc4790c4508927e97ef6c5d69a84b76987f74a58
Ronan C <rojocas@gmail.com>
2020-03-23 12:39:40 +0000

    Added payment processing in checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,8 +9,8 @@ class MakePaymentForm(forms.Form):
 
     credit_card_number = forms.CharField(label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.CharField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.CharField(label='Year', choices=YEAR_CHOICES, required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
@@ -19,4 +19,5 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'phone_number', 'country', 'postcode',
                   'town_or_city', 'street_address1', 'street_address2',
-                  'county')
+                  'county'
+                  )
