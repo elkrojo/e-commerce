3029886842d8e062eb6b0c78e6e46999ecea4598
Ronan C <rojocas@gmail.com>
2020-03-23 19:37:03 +0000

    Added S3 bucket for static files

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import url_reset
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
-    url(r'^register/', register, name="registration"),
+    url(r'^register/', register, name="register"),
     url(r'^profile/', user_profile, name="profile"),
     url(r'^password-reset/', include(url_reset)),
 ]
