08c3390061487b1f2f69633ab07655b8d4c03263
Ronan C <rojocas@gmail.com>
2020-03-24 16:03:27 +0000

    Turn off debug for production build

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if "DATABASE_URL" in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'ecommerce-rc80.herokuapp.com']
 
